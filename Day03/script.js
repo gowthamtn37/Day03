45a5550ccc90735a3389ba19407057155955bc2a
Gowtham M <108858082+gowthamtn37@users.noreply.github.com>
2022-07-13 19:22:15 +0530

    Add files via upload

---
+++ Day03/script.js
@@ -0,0 +1,46 @@
+// 1. how to compare two json have the same properties without order
+//a. var obj1 = {name:"person 1", age:5};
+//b. var obj2 = {age:5,name:"person 1" };
+//answer
+
+ var obj1 = {name:"person 1", age:5};
+ var obj2 = {age:5,name:"person 1" };
+
+ if(JSON.stringify(obj1) == JSON.stringify(obj2))
+ {
+    console.log("true");
+ }
+ else
+ {
+    console.log("false");
+ }    
+
+ 
+ // 2. use the rest countries API URL >https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json & display all the country flag
+//answer
+
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result)
+    for(var i=0; i<result.length; i++)
+    console.log(result[i].flags);
+
+}                                                     
+
+
+ // 3. use the same rest countries & print all countries name, region, subregion & population
+//answer
+
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result)
+    for(var i=0; i<result.length; i++)
+    console.log(result[i].name+" "+result[i].region+" "+result[i].subregion+" "+result[i].population);
+
+}                                                     
